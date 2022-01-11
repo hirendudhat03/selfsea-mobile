82d29ca5e75c1583a6bd2c9349e4bc9153c6b241
Anshuman <anshuman@Anshumans-MacBook-Pro.local>
2022-01-11 12:51:23 +0530

    Changes

--- app/component/Authentication.tsx
+++ app/component/Authentication.tsx
@@ -21,26 +21,14 @@ interface Props {
 }
 
 const Authentication = ({ text, icon, type }: Props) => {
-<<<<<<< HEAD
   let instagramLogin = useRef();
   const _signIn = async () => {
     console.log('handlePressGoogleLogin');
     GoogleSignin.configure({
       // androidClientId: '3A:84:C8:28:4A:5F:82:9F:12:8B:71:46:C9:87:0F:68:E6:38:7E:AE',
       iosClientId: '880711382534-k6q6jmtatddtll7u9qfmn31cbc1ckav1.apps.googleusercontent.com',
-=======
-  useEffect(() => {
-    GoogleSignin.configure({
-      // Mandatory method to call before calling signIn()
-      // scopes: ['https://www.googleapis.com/auth/drive.readonly'],
-      // Repleace with your webClientId
-      // Generated from Firebase console
-      webClientId:
-        '651815828852-ieos3aa4gougfirdnf52carf51q8v52v.apps.googleusercontent.com',
->>>>>>> ef236077ebd93104fc76e0e1e0ca9df370c0b34b
     });
 
-<<<<<<< HEAD
     // GoogleSignin.hasPlayServices().then((hasPlayService) => {
     //   if (hasPlayService) {
       try{
@@ -56,29 +44,6 @@ const Authentication = ({ text, icon, type }: Props) => {
         } else {
           alert(error.message)
         }
-=======
-  const _signIn = async () => {
-    console.log('handlePressGoogleLogin');
-    try {
-      await GoogleSignin.hasPlayServices({
-        // Check if device has Google Play Services installed
-        // Always resolves to true on iOS
-        showPlayServicesUpdateDialog: true,
-      });
-      console.error('GoogleSignin');
-      const userInfo = await GoogleSignin.signIn();
-      console.error('User Info --> ', userInfo);
-    } catch (error) {
-      console.error('Message', JSON.stringify(error));
-      if (error.code === statusCodes.SIGN_IN_CANCELLED) {
-        alert('User Cancelled the Login Flow');
-      } else if (error.code === statusCodes.IN_PROGRESS) {
-        alert('Signing In');
-      } else if (error.code === statusCodes.PLAY_SERVICES_NOT_AVAILABLE) {
-        alert('Play Services Not Available or Outdated');
-      } else {
-        alert(error.message);
->>>>>>> ef236077ebd93104fc76e0e1e0ca9df370c0b34b
       }
     //   }
     // }).catch((e) => {
@@ -88,7 +53,6 @@ const Authentication = ({ text, icon, type }: Props) => {
 
   const authLogin = () => {
     console.log('key : ', type);
-<<<<<<< HEAD
     if (type === Constant.authLogin.GOOGLE ) {
       _signIn();
     } else if (type === Constant.authLogin.INSTAGRAM) {
@@ -98,17 +62,6 @@ const Authentication = ({ text, icon, type }: Props) => {
       alert('Apple')
     }else {
       alert('null')
-=======
-
-    if (type === Constant.authLogin.GOOGLE) {
-      _signIn();
-    } else if (type === Constant.authLogin.INSTAGRAM) {
-      alert('instagram');
-    } else if (type === Constant.authLogin.APPLE) {
-      alert('Apple');
-    } else {
-      alert('null');
->>>>>>> ef236077ebd93104fc76e0e1e0ca9df370c0b34b
     }
   };
 
