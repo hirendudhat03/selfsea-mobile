599c725fe7075d39e60c200bd713594d34b9edea
Anshuman <anshuman@Anshumans-MacBook-Pro.local>
2022-02-22 17:38:55 +0530

    Renamed File to resolve Already Included file name Linting issue

--- app/redux/saga/index.ts
+++ app/redux/saga/index.ts
@@ -2,7 +2,7 @@ import { takeEvery, all } from 'redux-saga/effects';
 
 import { loginSaga } from './LoginSaga';
 //// eslint-disable-next-line
-import { signUpSaga } from './SignUpSaga';
+import { signUpSaga } from './SignupSaga';
 import { createProfileSaga } from './CreateProfileSaga';
 import { homeSaga } from './HomeSaga';
 import { menuSaga } from './MenuSaga';
